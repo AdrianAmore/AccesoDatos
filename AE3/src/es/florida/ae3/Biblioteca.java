747aed5e5a3839cef499d2cf985b745e2cd2b97f
AdrianAmore <adampe@floridauniversitaria.es>
2021-10-28 19:55:44 +0200

    AE3 acabada (falta documentar)

--- AE3/src/es/florida/ae3/Biblioteca.java
+++ AE3/src/es/florida/ae3/Biblioteca.java
@@ -1,10 +1,9 @@
 package es.florida.ae3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import org.w3c.dom.*;
-
-
 
 import javax.xml.parsers.*;
 import javax.xml.transform.OutputKeys;
@@ -12,19 +11,22 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPathConstants;
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
 import org.xml.sax.*;
+
 import org.w3c.dom.*;
 
 import java.io.*;
 
 public class Biblioteca {
 	static Scanner sc = new Scanner(System.in);
+	static ArrayList<Llibre> count = recuperarTots();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		int op = 0;
 		System.out.println("---OPCIONS---");
@@ -44,6 +46,7 @@ public class Biblioteca {
 			case 1:
 				ArrayList<Llibre> llibres = recuperarTots();
 				System.err.println("---\tLlibres actuals de la biblioteca\t---");
+				Thread.sleep(500);
 				for (Llibre l : llibres) {
 					System.out.println(l.getTitol() + " || Id: " + l.getIdentificador());
 				}
@@ -53,26 +56,51 @@ public class Biblioteca {
 				mostrarLlibre(recuperarLlibre(sc.nextInt()));
 				break;
 			case 3:
-				System.out.print("Identificador: ");
-				int id = sc.nextInt();
-				System.out.print("Titol: ");
-				String titl = sc.next();
-				System.out.print("Autor: ");
-				String aut = sc.next();
-				System.out.print("Any de publicació: ");
-				int any = sc.nextInt();
-				System.out.print("Editorial: ");
-				String edit = sc.next();
-				System.out.print("Numero de pàgines: ");
-				int nump = sc.nextInt();
-				Llibre llibre = new Llibre(id, titl, aut, any, edit, nump);
-				crearLlibre(llibre);
+				try {
+					System.out.print("Titol: ");
+					String titl = sc.next();
+					System.out.print("Autor: ");
+					String aut = sc.next();
+					System.out.print("Any de publicació: ");
+					int any = sc.nextInt();
+					System.out.print("Editorial: ");
+					String edit = sc.next();
+					System.out.print("Numero de pàgines: ");
+					int nump = sc.nextInt();
+					int id = count.size();
+					Llibre llibre = new Llibre(id, titl, aut, any, edit, nump);
+					int ll = crearLlibre(llibre);
+					Thread.sleep(100);
+					System.out.println("Llibre creat amb exit amb index: " + ll);
+				} catch (Exception e) {
+					System.err.println("Error al crear el llibre!");
+					e.printStackTrace();
+				}
+
 				break;
 			case 4:
+				System.out.print("Introdueix el index del llibre a editar: ");
+				int id = sc.nextInt();
+				try {
+					actualitzaLlibre(id);
+					Thread.sleep(100);
+					System.out.println("Llibre editat amb exit!");
+				} catch (Exception e) {
+					System.err.println("Error al editar el llibre!");
+					e.printStackTrace();
+				}
 				break;
 			case 5:
 				System.out.print("Introdueix el index del llibre a borrar: ");
-				borrarLlibre(sc.nextInt());
+				try {
+					borrarLlibre(sc.nextInt());
+					Thread.sleep(100);
+					System.out.println("Llibre borrat amb exit!\n\rATENCIÓ, ELS INDEX DELS LLIBRES HAN CAMBIAT! Comprovar amb l'opció 1");
+				} catch (Exception e) {
+					System.err.println("Error al borrar el llibre!");
+					e.printStackTrace();
+				}
+
 				break;
 			case 6:
 				System.out.println("Biblioteca tancada, fins la proxima.");
@@ -84,73 +112,133 @@ public class Biblioteca {
 		sc.close();
 	}
 
+	static void actualitzaLlibre(int identificador) {
+		Document dom;
+		Element e = null;
+		String xml = "biblioteca.xml";
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			dom = db.newDocument();
+
+			Element rootEle = dom.createElement("llibres");
+			ArrayList<Llibre> llibres = recuperarTots();
+			System.out.print("Introdueix el nou titol: ");
+			String tit = sc.next();
+			System.out.print("Introdueix el nou autor: ");
+			String aut = sc.next();
+			System.out.print("Introdueix el nou any de publicació: ");
+			String any = sc.next();
+			System.out.print("Introdueix la nova editorial: ");
+			String edit = sc.next();
+			System.out.print("Introdueix el nou numero de pàgines: ");
+			String nPag = sc.next();
+			Llibre g = new Llibre(identificador, tit, aut, Integer.parseInt(any), edit, Integer.parseInt(nPag));
+			llibres.set(identificador, g);
+			for (Llibre llib : llibres) {
+				Element l = dom.createElement("llibre");
+				rootEle.appendChild(l);
+				l.setAttribute("id", Integer.toString(llib.getIdentificador()));
+
+				e = dom.createElement("titol");
+				e.appendChild(dom.createTextNode(llib.getTitol()));
+				l.appendChild(e);
+
+				e = dom.createElement("autor");
+				e.appendChild(dom.createTextNode(llib.getAutor()));
+				l.appendChild(e);
+
+				e = dom.createElement("any-publi");
+				e.appendChild(dom.createTextNode(Integer.toString(llib.getAnyPubli())));
+				l.appendChild(e);
+
+				e = dom.createElement("editorial");
+				e.appendChild(dom.createTextNode(llib.getEditorial()));
+				l.appendChild(e);
+
+				e = dom.createElement("n-pag");
+				e.appendChild(dom.createTextNode(Integer.toString(llib.getNumPag())));
+				l.appendChild(e);
+
+			}
+			dom.appendChild(rootEle);
+
+			try {
+				Transformer tr = TransformerFactory.newInstance().newTransformer();
+				tr.setOutputProperty(OutputKeys.INDENT, "yes");
+				tr.setOutputProperty(OutputKeys.METHOD, "xml");
+				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+				tr.transform(new DOMSource(dom), new StreamResult(new FileOutputStream(xml)));
+			} catch (TransformerException te) {
+				System.out.println(te.getMessage());
+			} catch (IOException ioe) {
+				System.out.println(ioe.getMessage());
+			}
+		} catch (ParserConfigurationException pce) {
+			System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+		}
+	}
+
 	static int crearLlibre(Llibre llibre) {
-	    Document dom;
-	    Element e = null;
-	    String xml = "prueba.xml";
+		Document dom;
+		Element e = null;
+		String xml = "biblioteca.xml";
 
-	    // instance of a DocumentBuilderFactory
-	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	    try {
-	        // use factory to get an instance of document builder
-	        DocumentBuilder db = dbf.newDocumentBuilder();
-	        // create instance of DOM
-	        dom = db.newDocument();
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			dom = db.newDocument();
 
-	        // create the root element
-	        Element rootEle = dom.createElement("llibres");
-	        Element l = dom.createElement("llibre");
-	        rootEle.appendChild(l);
-	        l.setAttribute("id",Integer.toString(llibre.getIdentificador()));
-	        
-	        
+			Element rootEle = dom.createElement("llibres");
+			ArrayList<Llibre> llibres = recuperarTots();
+			llibres.add(llibre);
+			for (Llibre llib : llibres) {
+				Element l = dom.createElement("llibre");
+				rootEle.appendChild(l);
+				l.setAttribute("id", Integer.toString(llib.getIdentificador()));
 
-	        // create data elements and place them under root
-	        e = dom.createElement("titol");
-	        e.appendChild(dom.createTextNode(llibre.getTitol()));
-	        l.appendChild(e);
-	        
-	        e = dom.createElement("autor");
-	        e.appendChild(dom.createTextNode(llibre.getAutor()));
-	        l.appendChild(e);
+				e = dom.createElement("titol");
+				e.appendChild(dom.createTextNode(llib.getTitol()));
+				l.appendChild(e);
 
+				e = dom.createElement("autor");
+				e.appendChild(dom.createTextNode(llib.getAutor()));
+				l.appendChild(e);
 
+				e = dom.createElement("any-publi");
+				e.appendChild(dom.createTextNode(Integer.toString(llib.getAnyPubli())));
+				l.appendChild(e);
 
-	        e = dom.createElement("any-publi");
-	        e.appendChild(dom.createTextNode(Integer.toString(llibre.getAnyPubli())));
-	        l.appendChild(e);
+				e = dom.createElement("editorial");
+				e.appendChild(dom.createTextNode(llib.getEditorial()));
+				l.appendChild(e);
 
-	        e = dom.createElement("editorial");
-	        e.appendChild(dom.createTextNode(llibre.getEditorial()));
-	        l.appendChild(e);
-	        
-	        e = dom.createElement("n-pag");
-	        e.appendChild(dom.createTextNode(Integer.toString(llibre.getNumPag())));
-	        l.appendChild(e);
+				e = dom.createElement("n-pag");
+				e.appendChild(dom.createTextNode(Integer.toString(llib.getNumPag())));
+				l.appendChild(e);
 
-	        dom.appendChild(rootEle);
+			}
+			dom.appendChild(rootEle);
 
-	        try {
-	            Transformer tr = TransformerFactory.newInstance().newTransformer();
-	            tr.setOutputProperty(OutputKeys.INDENT, "yes");
-	            tr.setOutputProperty(OutputKeys.METHOD, "xml");
-	            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-	            tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
-	            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			try {
+				Transformer tr = TransformerFactory.newInstance().newTransformer();
+				tr.setOutputProperty(OutputKeys.INDENT, "yes");
+				tr.setOutputProperty(OutputKeys.METHOD, "xml");
+				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+				tr.transform(new DOMSource(dom), new StreamResult(new FileOutputStream(xml)));
 
-	            // send DOM to file
-	            tr.transform(new DOMSource(dom), 
-	                                 new StreamResult(new FileOutputStream(xml)));
-
-	        } catch (TransformerException te) {
-	            System.out.println(te.getMessage());
-	        } catch (IOException ioe) {
-	            System.out.println(ioe.getMessage());
-	        }
-	    } catch (ParserConfigurationException pce) {
-	        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
-	    }
-		return 1;
+			} catch (TransformerException te) {
+				System.out.println(te.getMessage());
+			} catch (IOException ioe) {
+				System.out.println(ioe.getMessage());
+			}
+		} catch (ParserConfigurationException pce) {
+			System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+		}
+		return llibre.getIdentificador();
 	}
 
 	static void mostrarLlibre(Llibre llibre) {
@@ -191,7 +279,63 @@ public class Biblioteca {
 	}
 
 	static void borrarLlibre(int identificador) {
+		Document dom;
+		Element e = null;
+		String xml = "biblioteca.xml";
 
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			dom = db.newDocument();
+
+			Element rootEle = dom.createElement("llibres");
+			ArrayList<Llibre> llibres = recuperarTots();
+
+			llibres.remove(identificador);
+			for (int i = 0; i < llibres.size(); i++) {
+				Element l = dom.createElement("llibre");
+				rootEle.appendChild(l);
+				l.setAttribute("id", Integer.toString(i));
+
+				e = dom.createElement("titol");
+				e.appendChild(dom.createTextNode(llibres.get(i).getTitol()));
+				l.appendChild(e);
+
+				e = dom.createElement("autor");
+				e.appendChild(dom.createTextNode(llibres.get(i).getAutor()));
+				l.appendChild(e);
+
+				e = dom.createElement("any-publi");
+				e.appendChild(dom.createTextNode(Integer.toString(llibres.get(i).getAnyPubli())));
+				l.appendChild(e);
+
+				e = dom.createElement("editorial");
+				e.appendChild(dom.createTextNode(llibres.get(i).getEditorial()));
+				l.appendChild(e);
+
+				e = dom.createElement("n-pag");
+				e.appendChild(dom.createTextNode(Integer.toString(llibres.get(i).getNumPag())));
+				l.appendChild(e);
+
+			}
+			dom.appendChild(rootEle);
+
+			try {
+				Transformer tr = TransformerFactory.newInstance().newTransformer();
+				tr.setOutputProperty(OutputKeys.INDENT, "yes");
+				tr.setOutputProperty(OutputKeys.METHOD, "xml");
+				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+				tr.transform(new DOMSource(dom), new StreamResult(new FileOutputStream(xml)));
+
+			} catch (TransformerException te) {
+				System.out.println(te.getMessage());
+			} catch (IOException ioe) {
+				System.out.println(ioe.getMessage());
+			}
+		} catch (ParserConfigurationException pce) {
+			System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+		}
 	}
 
 	static ArrayList<Llibre> recuperarTots() {
